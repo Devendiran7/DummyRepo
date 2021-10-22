79dfeb813552ef2407b0942dd06e811780408b6b
Devendiran7 <devendiran7699@gmail.com>
2021-10-22 20:12:16 +0530

    DevaCommit

--- src/test/java/org/test/SampleClass.java
+++ src/test/java/org/test/SampleClass.java
@@ -3,22 +3,24 @@ package org.test;
 import org.testng.annotations.Test;
 
 public class SampleClass {
-	
+
 	@Test
 	private void hello() {
 		System.out.println("hello");
 	}
-	
+
 	@Test
 	private void hello2() {
 		System.out.println("hello2");
 	}
-	
+
 	@Test
 	private void hello1() {
 		System.out.println("hello1");
 	}
-	
-	
+	@Test
+	private void newtest() {
+		System.out.println("new");
+	}
 
 }
